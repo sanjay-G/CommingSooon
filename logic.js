8c06f4bb712d326a825fa8d3906dc93696e8d7c2
Sanjay Govindhan <52875919+sanjay-G@users.noreply.github.com>
2023-08-09 17:56:58 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -3,7 +3,7 @@ function sendEmail()
     Email.send({
         Host: "smtp.gmail.com",
         Username : "lycheeprojects.in@gmail.com",
-        Password : "Lycheeprojects.1@",
+        Password : "Lycheeprojects.1",
         To : 'sanjayr112018@gmail.com',
         From : "lycheeprojects.in@gmail.com",
         Subject : "TestMail",

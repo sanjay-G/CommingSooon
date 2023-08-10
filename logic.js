5382fb3976bcbf8a454ba5e5557f195018c206eb
Sanjay <sanjayr112018@gmail.com>
2023-08-10 10:30:10 +0530

    added placeholder for emaiil box

--- logic.js
+++ logic.js
@@ -6,9 +6,15 @@ function sendEmail()
         Password : "38DACF91DD6A5DECBF1D98C134B88D1298A8",
         To : 'sanjayr112018@gmail.com',
         From : "sanjayr112018@gmail.com",
-        Subject : "TestMail",
-        Body : "tested ",
+        Subject : "Subscribed Mails-LycheeProjects",
+        Body : document.getElementById("inputMail").value,
     }).then(
 		message => alert(message)
 	);
 }
+
+
+// Elastic mail is used for smtp server
+//Settings--->
+// SMTP->create smtp crendiatals -> put a mail id only -> get a password no need to include port in the code 
+// Domains -> manage dmains -> i dont own domain (if you dont have domain for mail) -> verification 
